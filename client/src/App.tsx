7aeadb51246103b8dcc9f36ddf70ebe1cf86e0ef
ri4an <vadimguber7@gmail.com>
2022-07-21 15:25:48 +0600

    Fix: remove unused styles and tags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from './pages/Header/Header'
+import Content from './pages/Content/Content'
 import './App.css'
 
 function App() {
-    return <div className='App'></div>
+    return (
+        <div id='app'>
+            <Header />
+            <Content />
+        </div>
+    )
 }
 
 export default App
