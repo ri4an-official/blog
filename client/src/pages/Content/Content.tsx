911fcb646f6e9f990c49367780ae858caa85dfba
ri4an <vadimguber7@gmail.com>
2022-07-21 15:26:06 +0600

    Feature: add empty components

---
+++ client/src/pages/Content/Content.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Posts from '../../components/Posts/Posts'
+
+const Content = () => {
+    return (
+        <div>
+            <h1>Content</h1>
+            <Posts />
+        </div>
+    )
+}
+
+export default Content
