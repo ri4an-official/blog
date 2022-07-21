43211e2e672176444090fb46548622bab396ac83
ri4an <vadimguber7@gmail.com>
2022-07-21 21:26:47 +0600

    Feature: add post config

---
+++ client/src/module/post/api/config.ts
@@ -0,0 +1,6 @@
+import { BASE_URL } from './../../common/config'
+export const postsConfig = {
+    GET_ALL: `${BASE_URL}/posts`,
+    CREATE: BASE_URL,
+    UPDATE: BASE_URL,
+}
---
+++ client/src/module/post/types/Post.type.ts
@@ -0,0 +1,6 @@
+export interface Post {
+    id: number
+    text: string
+    date: string
+    authorId: string
+}
