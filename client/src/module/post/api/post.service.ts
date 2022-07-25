9508e6cf23f2eada8c3321566df28d6051a7610e
ri4an <vadimguber7@gmail.com>
2022-07-26 00:29:55 +0600

    Fix: update service

--- client/src/module/post/api/post.service.ts
+++ client/src/module/post/api/post.service.ts
@@ -1,40 +1,36 @@
 import { mapToCamelCase } from './../../common/helpers'
 import { HttpMethod } from './../../common/config'
 import { fetchExtended } from '../../common/helpers'
-import { Post } from '../types/Post.type'
+import { IPost } from '../types/Post.type'
 import { postsConfig } from './config'
 
 class PostService {
-    async create(post: Post): Promise<Post> {
-        const response = await fetchExtended(postsConfig.CREATE, HttpMethod.POST, {
-            ...post,
-        })
-        const newPost: Post = await response.json()
-        return mapToCamelCase(newPost)
-    }
+	async create(post: IPost): Promise<IPost> {
+		const response = await fetchExtended(postsConfig.CREATE, HttpMethod.POST, {
+			...post,
+		})
+		const newPost: IPost = await response.json()
+		return mapToCamelCase(newPost)
+	}
 
-    async update(post: Post): Promise<Post> {
-        const response = await fetchExtended(postsConfig.CREATE, HttpMethod.PUT, {
-            ...post,
-        }).then((r) => r.json())
-        const updatedPost: Post = await response.json()
-        return mapToCamelCase(updatedPost)
-    }
+	async update(post: IPost): Promise<IPost> {
+		const response = await fetchExtended(postsConfig.CREATE, HttpMethod.PUT, {
+			...post,
+		})
+		const updatedPost: IPost = await response.json()
+		return mapToCamelCase(updatedPost)
+	}
 
-    async getAll(): Promise<Post[]> {
-        const response = await fetchExtended(postsConfig.GET_ALL).then((r) =>
-            r.json()
-        )
-        const posts: Post[] = await response.json()
-        return posts.map(mapToCamelCase)
-    }
+	async getAll(): Promise<IPost[]> {
+		const response = await fetchExtended(postsConfig.GET_ALL)
+		const posts: IPost[] = await response.json()
+		return posts.map(mapToCamelCase)
+	}
 
-    async get(userId: number): Promise<Post> {
-        const response = await fetchExtended(
-            `${postsConfig.GET_ALL}/${userId}`
-        ).then((r) => r.json())
-        const post: Post = await response.json()
-        return mapToCamelCase(post)
-    }
+	async get(userId: number): Promise<IPost> {
+		const response = await fetchExtended(`${postsConfig.GET_ALL}/${userId}`)
+		const post: IPost = await response.json()
+		return mapToCamelCase(post)
+	}
 }
 export default new PostService()
