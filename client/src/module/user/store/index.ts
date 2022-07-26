a5187f3f2aa1b71eac73597a601aada18cb9c627
ri4an <vadimguber7@gmail.com>
2022-07-26 14:28:00 +0600

    Fix: add state for user

--- client/src/module/user/store/index.ts
+++ client/src/module/user/store/index.ts
@@ -1,2 +1,31 @@
-// export const getUser = createEffect(async (id: number) => await postService.getAll())
-export {}
+import { IUser, IUserData } from './../types/User.type'
+import { createEffect, createStore } from 'effector'
+import userService from '../api/user.service'
+import { UserDone, UserError } from '../types/Response.type'
+
+const checkFx = createEffect<string, UserDone, UserError>(
+	async (token: string) => await userService.check(token)
+)
+
+const refreshFx = createEffect(
+	async (token: string) => await userService.refresh(token)
+)
+
+const loginFx = createEffect(
+	async (user: IUserData) => await userService.login(user)
+)
+
+const initUser: IUser = {
+	user: null,
+	isAuth: false,
+}
+
+const $user = createStore<IUser>(initUser)
+	.on(checkFx.doneData, (user, resp) => ({
+		...user,
+		...resp.user,
+	}))
+	.on(checkFx.failData, (user, error) => ({
+		...user,
+		...error,
+	}))
