5583233bb14baaa94229d2d1cdb840971897402e
ri4an <vadimguber7@gmail.com>
2022-08-09 00:59:28 +0600

    Feature: add validation & hide password icon

--- client/src/components/auth/widget/Login.tsx
+++ client/src/components/auth/widget/Login.tsx
@@ -1,19 +1,30 @@
-import { Button, FormGroup, Grid, TextField } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { Button, FormGroup, Grid, IconButton, TextField } from '@mui/material'
 import { useStore } from 'effector-react'
 import { RoutePath } from 'entity/router'
 import { $isAuth, loginFx } from 'feature/auth'
+import { useMemo, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useInput } from 'shared/lib/hooks/useInput'
 
 const Login = () => {
 	const isAuth = useStore($isAuth)
-	const username = useInput()
-	const password = useInput()
+	const loginInp = useInput()
+	const passwordInp = useInput()
+
+	const [visible, setVisible] = useState(false)
+	const type = useMemo(() => (visible ? 'text' : 'password'), [visible])
+	const toggle = () => setVisible((v) => !v)
+
+	const disabled = useMemo(
+		() => loginInp.error || passwordInp.error,
+		[passwordInp.error, loginInp.error]
+	)
 
 	const submit = async () =>
 		await loginFx({
-			username: username.value,
-			password: password.value,
+			username: loginInp.value,
+			password: passwordInp.value,
 		})
 
 	return !isAuth ? (
@@ -31,28 +42,29 @@ const Login = () => {
 				<FormGroup>
 					<TextField
 						label='Username'
-						value={username.value}
-						onChange={username.onChange}
 						sx={{ mt: 2, mb: 2 }}
-						helperText={username.error}
-						error={!!username.error}
+						{...loginInp}
 					/>
 				</FormGroup>
 				<FormGroup>
 					<TextField
 						label='Password'
-						value={password.value}
-						onChange={password.onChange}
+						type={type}
 						sx={{ mb: 2 }}
-						type='password'
-						helperText={password.error}
-						error={!!password.error}
+						{...passwordInp}
 					/>
+					<IconButton
+						aria-label='toggle password visibility'
+						onClick={toggle}
+						edge='end'
+					>
+						{visible ? <VisibilityOff /> : <Visibility />}
+					</IconButton>
 				</FormGroup>
 				<Button
 					color='primary'
 					variant='outlined'
-					disabled={!!username.error || !!password.error}
+					disabled={disabled}
 					onClick={submit}
 				>
 					Sign In
--- client/src/shared/lib/hooks/useInput.ts
+++ client/src/shared/lib/hooks/useInput.ts
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export function useInput(initValue = '') {
 	const [value, setValue] = useState(initValue)
-	const [error, setError] = useState<null | string>(null)
+	const [helperText, setError] = useState<null | string>(null)
+	const error = useMemo(() => !!helperText, [helperText])
 
 	useEffect(() => {
-		if (value.length < 5) setError('Min 5')
-		else if (value.includes(' ')) setError('Without "Spaces"')
+		const valid = /^[a-zA-Z0-9]+$/
+		if (!value.match(valid)) setError('Incorrect input. Symbols A-Z, 0-9')
 		else setError(null)
 	}, [value])
 
 	return {
 		value,
 		onChange: (e: any) => setValue(e.currentTarget.value),
+		helperText,
 		error,
 	}
 }
